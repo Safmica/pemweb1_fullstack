b966a9b785d650e42d356f28ce20eececd3bd929
Safmica <safmikasa@gmail.com>
2024-10-30 16:47:29 +0700

    feat: animation for carousel and text home

--- js/index.js
+++ js/index.js
@@ -1,45 +1,135 @@
-function animateText(text, element) {
-    element.innerHTML = '';
-    const characters = text.split('');
-    characters.forEach((char, index) => {
-        setTimeout(() => {
-            element.innerHTML += char;
-        }, index * 50);
+(function (document) {
+  const markers = [...document.querySelectorAll('mark')];
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.style.animationPlayState = 'running';
+        observer.unobserve(entry.target);
+      }
     });
+  }, {
+    threshold: 0.8
+  });
+  
+  markers.forEach(mark => {
+    observer.observe(mark);
+  });
+})(document);
+
+//curtains
+function animateText(text, element) {
+  element.innerHTML = '';
+  const characters = text.split('');
+  characters.forEach((char, index) => {
+    setTimeout(() => {
+      element.innerHTML += char;
+    }, index * 50);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const fadeInElement = document.getElementById('fadeInElement');
-    fadeInElement.classList.add('active');
-});
+const words = ['WELCOME TO ERSANAWATA HIGH SCHOOL'];
+let part, i = 0, offset = 0, len = words.length, forwards = true, skip_count = 0, skip_delay = 15, speed = 70;
+
+const wordflick = () => {
+  setInterval(() => {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count === skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset === 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) i = 0;
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count === 0) {
+      if (forwards) offset++;
+      else offset--;
+    }
+  }, speed);
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-    const titleElement = document.getElementById('animatedTitle');
-    document.body.classList.add('open');
+  wordflick();
 
-    animateText("Welcome To ERSANAWATA Official Website", titleElement);
+  const fadeInElement = document.getElementById('fadeInElement');
+  fadeInElement.classList.add('active');
+
+  const titleElement = document.getElementById('animatedTitle');
+  document.body.classList.add('open');
+  animateText("Welcome To ERSANAWATA Official Website", titleElement);
 });
+
+const quote = ["Building", "Shaping", "Empowering", "Inspiring"];
+const colors = ["blue", "green", "yellow", "red"];
+const text = document.querySelector(".text");
+
+function* generator() {
+  let index = 0;
+  while (true) {
+    yield index++;
+    if (index > 3) index = 0;
+  }
+}
+
+function printChar(word) {
+  let i = 0;
+  text.innerHTML = "";
+  text.classList.add(colors[quote.indexOf(word)]);
+  const id = setInterval(() => {
+    if (i >= word.length) {
+      deleteChar();
+      clearInterval(id);
+    } else {
+      text.innerHTML += word[i];
+      i++;
+    }
+  }, 300);
+}
+
+function deleteChar() {
+  const word = text.innerHTML;
+  let i = word.length - 1;
+  const id = setInterval(() => {
+    if (i >= 0) {
+      text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
+      i--;
+    } else {
+      text.classList.remove(colors[quote.indexOf(word)]);
+      printChar(quote[gen.next().value]);
+      clearInterval(id);
+    }
+  }, 200);
+}
+
+const gen = generator();
+printChar(quote[gen.next().value]);
 
 function animateValue(id, start, end, duration, ext) {
-    let startTime = null;
+  let startTime = null;
 
-    const animate = (timestamp) => {
-        if (!startTime) startTime = timestamp;
-        const progress = timestamp - startTime;
-        const currentValue = Math.floor(
-            progress / duration * (end - start) + start
-        );
+  const animate = (timestamp) => {
+    if (!startTime) startTime = timestamp;
+    const progress = timestamp - startTime;
+    const currentValue = Math.floor((progress / duration) * (end - start) + start);
+    document.getElementById(id).innerText = currentValue + ext;
 
-        document.getElementById(id).innerText = currentValue + ext;
+    if (progress < duration) {
+      requestAnimationFrame(animate);
+    } else {
+      document.getElementById(id).innerText = end + ext;
+    }
+  };
 
-        if (progress < duration) {
-            requestAnimationFrame(animate);
-        } else {
-            document.getElementById(id).innerText = end + ext;
-        }
-    };
-
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 
 animateValue("achievement-1", 0, 97, 2000, '%');
